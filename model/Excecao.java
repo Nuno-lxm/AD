d0759495de2b1d005b16eb34cd320ed53271b898
Nuno-lxm <nunolxm@hotmail.com>
2024-11-26 00:58:14 +0000

    Delete model directory

--- model/Excecao.java
+++
@@ -1,9 +0,0 @@
-package projeto.model;
-
-import java.io.Serializable;
-
-public class Excecao extends java.lang.Exception implements Serializable {
-    public Excecao(String message) {
-        super(message);
-    }
-}
